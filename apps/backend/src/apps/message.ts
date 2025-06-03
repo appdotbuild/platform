eb545d514be9a9eb81025bffd9e6b57ab4a997fb
Mario Cadenas <mario@neon.tech>
2025-06-03 21:31:28 +0200

    feat: deployment polling

--- apps/backend/src/apps/message.ts
+++ apps/backend/src/apps/message.ts
@@ -516,7 +516,7 @@ export async function postMessage(
                 isPermanentApp = true;
               }
 
-              const { appURL } = await writeMemfsToTempDir(
+              const { appURL, deploymentId } = await writeMemfsToTempDir(
                 memfsVolume,
                 virtualDir,
               ).then((tempDirPath) =>
@@ -535,10 +535,13 @@ export async function postMessage(
 
               session.push(
                 new PlatformMessage(
-                  AgentStatus.IDLE,
+                  AgentStatus.RUNNING,
                   traceId as TraceId,
-                  `Your application has been deployed to ${appURL}`,
-                  { type: PlatformMessageType.DEPLOYMENT_COMPLETE },
+                  `Your application is being deployed to ${appURL}`,
+                  {
+                    type: PlatformMessageType.DEPLOYMENT_IN_PROGRESS,
+                    deploymentId,
+                  },
                 ),
               );
             }
